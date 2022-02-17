af1f329f5356c1eb2befba3ec0cd8505d523d41d
lucas <lucas.robitaille@yahoo.com>
2022-02-17 13:58:05 -0800

    [v1] updated footer

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -1,11 +1,32 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, ButtonGroup, IconButton } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Flex as="footer" width="full" align="center">
+    <Flex as="footer" width="full" align="center" justify="space-between">
       <Text color="gray.500">
         © {new Date().getFullYear()} - Lucas Robitaille
       </Text>
+      <ButtonGroup variant="ghost">
+        <IconButton
+          as="a"
+          href="mailto:lucas.robitaille@yahoo.com"
+          aria-label="Email Me"
+          icon={<FaEnvelope fontSize="20px" />}
+        />
+        <IconButton
+          as="a"
+          href="https://www.linkedin.com/in/lucas-robitaille/"
+          aria-label="Linkedin"
+          icon={<FaLinkedin fontSize="20px" />}
+        />
+        <IconButton
+          as="a"
+          href="https://github.com/robitaille1"
+          aria-label="Github"
+          icon={<FaGithub fontSize="20px" />}
+        />
+      </ButtonGroup>
     </Flex>
   );
 };
