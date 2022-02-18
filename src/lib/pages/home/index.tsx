d65d74d1d34c2efa7099f7f7232e9eaab8169ec5
lucas <lucas.robitaille@yahoo.com>
2022-02-18 08:12:16 -0800

    [v1] updated home to reflect new sections

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,25 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 
-import CTASection from "lib/components/samples/CTASection";
-import SomeImage from "lib/components/samples/SomeImage";
-import SomeText from "lib/components/samples/SomeText";
+import Hero from "../../components/Hero";
+import Tech from "../../components/Tech";
+import Experience from "../../components/Experience";
 
 const Home = () => {
   return (
-    <Box
-      display={{ md: "flex" }}
-      alignItems="center"
-      minHeight="70vh"
-      gap={8}
-      mb={8}
-      w="full"
-    >
-      <SomeImage />
-
-      <Box>
-        <SomeText />
-        <CTASection />
-      </Box>
+    <Box display={{ md: "flex" }} minHeight="70vh" w="full">
+      <Stack>
+        <Hero />
+        <Tech />
+        <Experience />
+      </Stack>
     </Box>
   );
 };
