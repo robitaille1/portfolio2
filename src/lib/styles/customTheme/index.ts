c042788af75a424c3b5abaccad3ca07d18c85625
Lucas <lucas@robitaille.xyz>
2023-05-11 16:52:18 -0700

    remove

--- src/lib/styles/customTheme/index.ts
+++ src/lib/styles/customTheme/index.ts
@@ -1,7 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import colors from "./colors";
-import Button from "./components/button";
 import fonts from "./fonts";
 
 const customTheme = extendTheme({
@@ -11,9 +10,6 @@ const customTheme = extendTheme({
   },
   fonts,
   colors,
-  components: {
-    Button,
-  },
 });
 
 export default customTheme;
